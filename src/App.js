40501e8be42a124528a4cda8a8ca1cdd977233c1
rizkianakbar <rizkianakbar4@gmail.com>
2021-03-08 17:11:20 +0700

    LEARN : useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useForm } from "./useForm";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [values, handleChange] = useForm({ email: "", password: "" });
+
   return (
-    <div>
-      Hello World!
+    <div style={{ textAlign: "center" }}>
+      <div>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+        <div>{count}</div>
+      </div>
+
+      <input
+        type="text"
+        value={values.email}
+        name="email"
+        onChange={handleChange}
+        placeholder="Enter your email"
+      />
+      <input
+        type="password"
+        value={values.password}
+        name="password"
+        onChange={handleChange}
+        placeholder="Enter your password"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
